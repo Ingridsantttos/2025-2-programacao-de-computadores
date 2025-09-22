7347fa1cfe8e60a49d9263d59db5a1e50c24fc6d
Ingridsantttos <ingrhyds@gmail.com>
2025-09-22 20:31:43 -0300

    Dados - print

---
+++ 22-09-2025/exemplo-02-print.py
@@ -0,0 +1,9 @@
+nome = 'ingrid
+
+idade = 19
+
+# Olá Ingrid, Você tem 19 anos.
+print('Olá', nome, ', você tem', idade, 'anos.')
+
+# Olá Ingrid, Você tem 19 anos.
+print(f'Olá {nome}, você tem', idade, anos)
