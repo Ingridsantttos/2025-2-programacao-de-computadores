ab7b86f394a39f66ca12584fcb0b55f1d028b673
Ingridsantttos <ingrhyds@gmail.com>
2025-09-23 21:43:46 -0300

    EX-01

--- 22-09-2025/exemplo-01-print.py
+++ 22-09-2025/exemplo-01-print.py
@@ -1,1 +1,12 @@
-print('olá mundo!!!!!')
+ano_atual   +2025
+ano_nascimento  = 2006
+
+idade   = ano-atual - ano_nascimento
+
+#print(idade)
+
+#print('você tem', idade, 'anos.')
+
+#print('você tem', + str(idade) + 'anos.')
+
+print(f'você tem {idade} anos.')
